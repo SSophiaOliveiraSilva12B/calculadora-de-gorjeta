003cb37197eaac504132c45594824ce575baa1db
SSophiaOliveiraSilva12B silva <sophialorynsoliveira@gmail.com>
2025-04-06 23:52:19 +0000

    estilização da seção de informações finalizada

---
+++ main.js
@@ -0,0 +1,13 @@
+let bill = 0
+let tipPorsentage = 0
+let numberOfpeople = 0
+
+function receiveBillValue() {
+    bill = document.querySelector("#bill").valueAsNmber
+
+    console.log(bill)
+}
+
+function receiveNumberOfPeopleValue() {
+    numberOfpeople = document.querySelector("#people").valueAsNmber
+}
