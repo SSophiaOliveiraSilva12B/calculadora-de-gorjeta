3744a98bd02bbe1db419f7be37ccd0edcf367a4f
SSophiaOliveiraSilva12B silva <sophialorynsoliveira@gmail.com>
2025-04-07 02:42:10 +0000

    criação da função para calcular os resultados

--- main.js
+++ main.js
@@ -1,41 +1,87 @@
-let bill = 0
-let tipPorsentage = 0
-let numberOfpeople = 0
-let buttonSelected = null
+let bill = 0;
+let tipPercentage = 0;
+let numberOfPeople = 0;
+let buttonSelected = null;
 
+let billInput = document.querySelector('#bill');
+billInput.addEventListener("input", reciveBillValue);
 
+let numberOfPeopleInput = document.querySelector('#people');
+numberOfPeopleInput.addEventListener("input", reciveNumberOfPeopleValue);
 
-function receiveBillValue() {
-    bill = document.querySelector("#bill").valueAsNmber
-
-    console.log(bill)
+function reciveBillValue() {
+    bill = billInput.valueAsNumber;
+    calculateResults();
 }
 
-function receiveNumberOfPeopleValue() {
-    numberOfpeople = document.querySelector("#people").valueAsNmber
+function reciveNumberOfPeopleValue() {
+    numberOfPeople = numberOfPeopleInput.valueAsNumber;
+    calculateResults();
 }
 
-function receiveTipPercentageValue(value) {
-    tipPorsentage = value / 100
+function reciveTipPercentageValue(value) {
+    tipPercentage = value / 100;
 
-    removeClassButtonSelected()
+    removeClassButtonSelected();
 
-    document.querySelector("#custom-tip").value = ""
+    document.querySelector("#custom-tip").value = "";
 
-    buttonSelected = document.querySelector(`#button-${value}`)
-    buttonSelected.classList.add("button-selected")
+    buttonSelected = document.querySelector(`#button-${value}`);
+    if (buttonSelected) {
+        buttonSelected.classList.add("button-selected");
+    }
+
+    calculateResults();
 }
 
+function reciveCustomTipPercentageValue() {
+    tipPercentage = document.querySelector("#custom-tip").valueAsNumber / 100;
 
-function reciveCustomTipPercentageValue () {
-    tipPorsentage = document.querySelector("#custom-tip").valueAsNmber / 100
+    removeClassButtonSelected();
 
-    removeClassButtonSelected()
+    calculateResults();
 }
 
 function removeClassButtonSelected() {
     if (buttonSelected !== null) {
-        buttonSelected.classList.remove("button-selected")
-        buttonSelected = null 
+        buttonSelected.classList.remove("button-selected");
+        buttonSelected = null;
     }
 }
+
+function calculateResults() {
+    if (bill !== 0 && tipPercentage !== 0 && numberOfPeople !== 0) {
+        let tipAmountPerson = calculateTipAmountPerson();
+        calculateTotalPerson(tipAmountPerson);
+    }
+}
+
+function calculateTipAmountPerson() {
+    let tipAmountStrong = document.querySelector(".amount strong");
+    let tipAmountPerson = bill * tipPercentage / numberOfPeople;
+    tipAmountStrong.textContent = `$${tipAmountPerson.toFixed(2)}`;
+    return tipAmountPerson;
+}
+
+function calculateTotalPerson(tipAmountPerson) {
+    let totalStrong = document.querySelector(".total strong");
+    let totalAmountPerson = (bill / numberOfPeople) + tipAmountPerson;
+    totalStrong.innerText = `$${totalAmountPerson.toFixed(2)}`;
+}
+
+function reset() {
+    billInput.value = "";
+    bill = 0;
+
+    tipPercentage = 0;
+    removeClassButtonSelected();
+    document.querySelector("#custom-tip").value = "";
+
+    numberOfPeopleInput.value = "";
+    numberOfPeople = 0;
+
+    document.querySelector(".amount strong").innerText = "$0.00";
+    document.querySelector(".total strong").innerText = "$0.00";
+}
+
+
