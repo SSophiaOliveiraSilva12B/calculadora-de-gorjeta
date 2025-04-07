9e3fd10f73f3e976b635356d4cd8c0c065eba991
SSophiaOliveiraSilva12B silva <sophialorynsoliveira@gmail.com>
2025-04-07 02:51:34 +0000

    funções para resetar todas as informações da calculadora

--- main.js
+++ main.js
@@ -1,87 +1,87 @@
-let bill = 0;
-let tipPercentage = 0;
-let numberOfPeople = 0;
-let buttonSelected = null;
+let bill = 0
+let tipPercentage = 0
+let numberOfPeople = 0
+let buttonSelected = null
 
-let billInput = document.querySelector('#bill');
-billInput.addEventListener("input", reciveBillValue);
+let billInput = document.querySelector('#bill')
+billInput.addEventListener("input", reciveBillValue)
 
-let numberOfPeopleInput = document.querySelector('#people');
-numberOfPeopleInput.addEventListener("input", reciveNumberOfPeopleValue);
+let numberOfPeopleInput = document.querySelector('#people')
+numberOfPeopleInput.addEventListener("input", reciveNumberOfPeopleValue)
 
 function reciveBillValue() {
-    bill = billInput.valueAsNumber;
-    calculateResults();
+    bill = billInput.valueAsNumber
+    calculateResults()
 }
 
 function reciveNumberOfPeopleValue() {
-    numberOfPeople = numberOfPeopleInput.valueAsNumber;
-    calculateResults();
+    numberOfPeople = numberOfPeopleInput.valueAsNumber
+    calculateResults()
 }
 
 function reciveTipPercentageValue(value) {
-    tipPercentage = value / 100;
+    tipPercentage = value / 100
 
-    removeClassButtonSelected();
+    removeClassButtonSelected()
 
-    document.querySelector("#custom-tip").value = "";
+    document.querySelector("#custom-tip").value = ""
 
-    buttonSelected = document.querySelector(`#button-${value}`);
+    buttonSelected = document.querySelector(`#button-${value}`)
     if (buttonSelected) {
-        buttonSelected.classList.add("button-selected");
+        buttonSelected.classList.add("button-selected")
     }
 
-    calculateResults();
+    calculateResults()
 }
 
 function reciveCustomTipPercentageValue() {
-    tipPercentage = document.querySelector("#custom-tip").valueAsNumber / 100;
+    tipPercentage = document.querySelector("#custom-tip").valueAsNumber / 100
 
-    removeClassButtonSelected();
+    removeClassButtonSelected()
 
-    calculateResults();
+    calculateResults()
 }
 
 function removeClassButtonSelected() {
     if (buttonSelected !== null) {
-        buttonSelected.classList.remove("button-selected");
-        buttonSelected = null;
+        buttonSelected.classList.remove("button-selected")
+        buttonSelected = null
     }
 }
 
 function calculateResults() {
     if (bill !== 0 && tipPercentage !== 0 && numberOfPeople !== 0) {
-        let tipAmountPerson = calculateTipAmountPerson();
-        calculateTotalPerson(tipAmountPerson);
+        let tipAmountPerson = calculateTipAmountPerson()
+        calculateTotalPerson(tipAmountPerson)
     }
 }
 
 function calculateTipAmountPerson() {
-    let tipAmountStrong = document.querySelector(".amount strong");
-    let tipAmountPerson = bill * tipPercentage / numberOfPeople;
-    tipAmountStrong.textContent = `$${tipAmountPerson.toFixed(2)}`;
-    return tipAmountPerson;
+    let tipAmountStrong = document.querySelector(".amount strong")
+    let tipAmountPerson = bill * tipPercentage / numberOfPeople
+    tipAmountStrong.textContent = `$${tipAmountPerson.toFixed(2)}`
+    return tipAmountPerson
 }
 
 function calculateTotalPerson(tipAmountPerson) {
-    let totalStrong = document.querySelector(".total strong");
-    let totalAmountPerson = (bill / numberOfPeople) + tipAmountPerson;
-    totalStrong.innerText = `$${totalAmountPerson.toFixed(2)}`;
+    let totalStrong = document.querySelector(".total strong")
+    let totalAmountPerson = (bill / numberOfPeople) + tipAmountPerson
+    totalStrong.innerText = `$${totalAmountPerson.toFixed(2)}`
 }
 
 function reset() {
-    billInput.value = "";
-    bill = 0;
+    bill = 0
+    document.querySelector("#bill").value = ""
 
-    tipPercentage = 0;
-    removeClassButtonSelected();
-    document.querySelector("#custom-tip").value = "";
+    tipPercentage = 0
+    removeClassButtonSelected()
+    document.querySelector("#custom-tip").value = ""
 
-    numberOfPeopleInput.value = "";
-    numberOfPeople = 0;
+    numberOfPeople = 0
+    document.querySelector("#people").value = ""
 
-    document.querySelector(".amount strong").innerText = "$0.00";
-    document.querySelector(".total strong").innerText = "$0.00";
+    document.querySelector(".amount strong").innerText = "$0.00"
+    document.querySelector(".total strong").innerText = "$0.00"
 }
 
 
